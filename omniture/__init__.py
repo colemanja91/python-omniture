42ce1b8d156de5a26bc2896db83628cb9ac0d0c5
colemanja91 <colemanja91@gmail.com>
2017-03-22 08:54:41 -0400

    fix relative imports for python3

--- omniture/__init__.py
+++ omniture/__init__.py
@@ -1,20 +1,24 @@
+""" omniture.__init__ """
 # encoding: utf-8
 
-from account import Account, Suite
-from elements import Value, Element, Segment
-from query import Query
-from reports import InvalidReportError, Report, OverTimeReport, \
+from .account import Account, Suite
+from .elements import Value, Element, Segment
+from .query import Query
+from .reports import InvalidReportError, Report, OverTimeReport, \
     RankedReport, TrendedReport, DataWarehouseReport
+from .utils import affix
 
 
 def authenticate(username, secret=None, endpoint=Account.DEFAULT_ENDPOINT, prefix='', suffix=''):
-    # if no secret is specified, we will assume that instead 
-    # we have received a dictionary with credentials (such as
-    # from os.environ)
+    """
+    if no secret is specified, we will assume that instead
+    we have received a dictionary with credentials (such as
+    from os.environ)
+    """
     if not secret:
         source = username
-        key_to_username = utils.affix(prefix, 'OMNITURE_USERNAME', suffix)
-        key_to_secret = utils.affix(prefix, 'OMNITURE_SECRET', suffix)
+        key_to_username = affix(prefix, 'OMNITURE_USERNAME', suffix)
+        key_to_secret = affix(prefix, 'OMNITURE_SECRET', suffix)
         username = source[key_to_username]
         secret = source[key_to_secret]
 
@@ -22,6 +26,9 @@ def authenticate(username, secret=None, endpoint=Account.DEFAULT_ENDPOINT, prefi
 
 
 def queue(queries):
+    """
+    queue queries
+    """
     if isinstance(queries, dict):
         queries = queries.values()
 
@@ -31,12 +38,12 @@ def queue(queries):
 
 def sync(queries, heartbeat=None, interval=1):
     """
-    `omniture.sync` will queue a number of reports and then 
+    `omniture.sync` will queue a number of reports and then
     block until the results are ready.
 
-    Queueing reports is idempotent, meaning that you can also 
-    use `omniture.sync` to fetch the results for queries that 
-    have already been queued: 
+    Queueing reports is idempotent, meaning that you can also
+    use `omniture.sync` to fetch the results for queries that
+    have already been queued:
 
         query = mysuite.report.range('2013-06-06').over_time('pageviews', 'page')
         omniture.queue(query)
